cab90297faa82aae8f1e6d60b35609b1470473d3
Justin Phillips <justinphil02@yahoo.com>
2017-09-07 00:33:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,5 +21,12 @@ def initialize(board = nil)
     end
   end
 end
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@baord[6]} | #{@board[7]} | #{@board[8]} "
+end
 
 end
